b80acde06d00d80a4bf06e7e1d81c02e8e6bca52
max-programming <ui.sabuwala@gmail.com>
2022-07-31 14:25:38 +0530

    Add CORS Middleware to API

--- api/new_app.py
+++ api/new_app.py
@@ -1,9 +1,20 @@
 from typing import Optional
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from prisma import Prisma
 from pydantic import BaseModel
 
 app = FastAPI()
+
+origins = ['http://localhost:5173']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods='*',
+    allow_headers='*',
+)
 
 
 class CreateTask(BaseModel):
